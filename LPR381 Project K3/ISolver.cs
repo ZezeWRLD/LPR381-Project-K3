1623c60522e0a01b3e8795e9052614d1d2f0a73a
ZezeWRLD <mthokozisin699@gmail.com>
2025-08-19 12:05:31 +0200

    Project Classes

---
+++ LPR381 Project K3/ISolver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPR381_Project_K3
+{
+    internal class ISolver
+    {
+    }
+}
---
+++ LPR381 Project K3/LPmodel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPR381_Project_K3
+{
+    internal class LPmodel
+    {
+        private string OptimizationType;
+        private List<double> ObjectiveCoef;
+        private List<List<double>> COnstraintCoef;
+        private List<string> ConstraintRel;
+        private List<double> ConstraintRHS;
+        private List<string> SignRes;
+
+
+    }
+}
