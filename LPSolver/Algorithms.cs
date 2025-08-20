7d31294326878b6873414d7f48af1479be019fd6
ZezeWRLD <mthokozisin699@gmail.com>
2025-08-20 19:41:16 +0200

    Architecture re-build

---
+++ LPSolver/Algorithms.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPSolver
+{
+    internal class Algorithms
+    {
+        public void PSimplexMethod(LPmodel model)
+        {
+            
+
+        }
+
+        public void RPSimplexMethod(LPmodel model)
+        {
+            
+        }
+
+        public void BandBSimplexMethod(LPmodel model)
+        {
+            
+        }
+
+        public void CPlaneMethod(LPmodel model)
+        {
+            
+        }
+
+        public void BandBKnapsackMethod(LPmodel model)
+        {
+            
+        }
+
+    }
+}
